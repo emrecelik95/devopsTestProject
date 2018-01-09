8b90e007942d24c62a46514c21c593463ecc2edc
Emre Çelik <33811689+emrecelik95@users.noreply.github.com>
2018-01-09 20:00:58 +0300

    Update Main.java

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,21 +1,42 @@
-
-/**
- * Hello world!
- *
- */
- 
+package com.mycompany.app;
 import java.io.*;
 public class Main 
 {
     public static void main( String[] args )throws Exception
     {
     	try{
-    		PrintWriter writer = new PrintWriter("test_out.txt", "UTF-8");
-			writer.println("Ornek Dosya , Deployment");
-			writer.close();
-		}catch (Exception e)
-		{
-		}
+		PrintWriter writer = new PrintWriter("test_out.txt", "UTF-8");
+		writer.println("Ornek Dosya , Deployment");
+		writer.close();
+	}catch (Exception e)
+	{
+	}
 		
     }
+
+    public static int sum(int a, int b) {
+    	return a+b;
+    }
+
+    public static int sub(int a, int b) {
+    	return a-b;
+    }
+
+    public static int mult(int a, int b) {
+   	return a*b;
+    }
+ 
+    public static double sumDouble (double a, double b){
+    	return a + b ;
+    }
+ 
+    public static String retString(String a){
+    	return a;
+    }
+ 
+    public static String StringAdder(String a,String b){
+    	return a+b;
+    }
+
+
 }
